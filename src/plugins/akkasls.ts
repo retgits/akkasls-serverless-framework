ea4e131f7ab3717309cf42eb0b43f91d8cb371b5
retgits <lstigter@gmail.com>
2021-07-05 15:33:35 -0700

    chore: use get.sh script to install akkasls

--- src/plugins/akkasls.ts
+++ src/plugins/akkasls.ts
@@ -62,45 +62,14 @@ export class AkkaServerlessCliPlugin extends BasePlugin {
      * @param {string} [location] The location to move the CLI to if it already exists
      */
     private _downloadAkkaSlsUnix(os: string, location?: string): void {
-        // TODO: Once Akka Serverless supports Apple M1 chips, update this call to support that too using `process.arch`
-        let result = run(`curl -OL https://downloads.akkaserverless.com/stable/akkasls_${os}_amd64.tar.gz`, join(homedir(), 'Downloads'));
+        const result = exec('curl -sL https://developer.lightbend.com/docs/akka-serverless/get.sh | bash', {
+            env: process.env,
+            async: false,
+        });
 
         if (result.code !== 0) {
             this.error('Failed to download the Akka Serverless CLI...');
             return;
         }
-
-        result = run(`tar xzf akkasls_${os}_amd64.tar.gz`, join(homedir(), 'Downloads'));
-
-        if (result.code !== 0) {
-            this.error('Unable to extract the Akka Serverless CLI...');
-            return;
-        }
-
-        if (location) {
-            this.warn(`Downloaded the Akka Serverless CLI to ${join(homedir(), 'Downloads')}. You have to move it to your PATH to use it`);
-            return;
-        }
-
-        result = run(`mv akkasls ${location}`, join(homedir(), 'Downloads'));
-        if (result.code !== 0) {
-            this.error('Unable to override the existing Akka Serverless CLI...');
-            return;
-        }
     }
-}
-
-/**
- * Executes the given command synchronously.
- *
- * @param {string} command The command to execute
- * @param {string} workingDir The directory to execute the command in
- * @return {*}  {ShellString} The response from the command
- */
-function run(command: string, workingDir: string): ShellString {
-    return exec(command, {
-        env: process.env,
-        async: false,
-        cwd: workingDir
-    });
 }
